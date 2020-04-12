51386aeda94028d92a5ef5e2ac00bb6698b51e5f
Pawel Cybulski <kontakt@pawelcybulski.pl>
2020-04-12 20:19:58 +0200

    computer info, mailing added

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 email = {
-    'email': 'kontakt@pawelcybulski.pl',
+    'email_from': 'scraper@pawelcybulski.pl',
+    'email_to': 'kontakt@pawelcybulski.pl',
     'password': '',
-    'port': 587
+    'port': 587,
+    'smtp': 'smtp.cba.pl'
 }
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-import multiprocessing
 import smtplib
+import config
+import os
 
 def log_to_file(log: str):
     with open('logs.txt', 'a') as logs_file:
@@ -8,9 +9,30 @@ def log_to_file(log: str):
 
 
 def get_computer_info():
-    cores_amount = multiprocessing.cpu_count()
+    computer_name = os.environ['COMPUTERNAME']
+    cores_amount = os.environ['NUMBER_OF_PROCESSORS']
+    os_system = os.environ['OS']
 
-    # name, cores, rams, network speed
+    return cores_amount, computer_name, os_system
+
 
 def send_email():
-    msmtObj = smtplib.SMTP('')
+    smtpObj = smtplib.SMTP(config.email['smtp'], config.email['port'])
+    print(str(smtpObj.ehlo()))
+
+    smtpObj.starttls()
+
+    smtpObj.login(config.email['email_from'], config.email['password'])
+
+    smtpObj.sendmail(config.email['email_from'], config.email['email_to'], 'Subject: So long.\nTest message from scraper')
+
+    smtpObj.close()
+
+get_computer_info()
+
+email_body = """
+Subject: Świat Książki Scraper \n
+Computer specification:
+
+
+"""
